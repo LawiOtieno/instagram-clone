6091f283c65f06713c3222bc6ba55e3b649226ff
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-19 07:31:30 +0300

    created models image

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
+
+
+class Image(models.Model):
+  photo = CloudinaryField('image')
+  photo_name = models.CharField(max_length=60)
+  posted_at = models.DateTimeField(auto_now_add=True)
+  photo_caption = models.TextField()
+  user = models.ForeignKey(User,on_delete = models.CASCADE)
+
+  def save_photo(self):
+     self.save()
+
+  
+
