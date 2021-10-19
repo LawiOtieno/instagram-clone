876822f45b4b013a19d9ba1da84a5d2641e8d17e
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-19 07:26:31 +0300

    linked urls and views

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,4 +7,12 @@ from . import views
 
 urlpatterns = [
     path('index/',views.index,name='home'),
+
+    path('accounts/register/',app_views.register,name='register'),
+    path('',auth_views.LoginView.as_view(template_name = 'registration/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html'),name='logout'),
+    path('accounts/profile/',views.profile,name='profile'),
+    path('update/',app_views.update_profile,name='update_profile'),
+    path('post/',app_views.post,name='post'),
+    path('photo/<int:photo_id>',views.detail,  name='photo.detail'),
 ]
