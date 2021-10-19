d96f0e8bbf0921ce97cd2cfcd19d05e4b9d23636
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-19 07:27:37 +0300

    linked urls redirect and views

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,4 +15,13 @@ urlpatterns = [
     path('update/',app_views.update_profile,name='update_profile'),
     path('post/',app_views.post,name='post'),
     path('photo/<int:photo_id>',views.detail,  name='photo.detail'),
+
+    re_path(r'^comment/(?P<photo_id>\d+)$',app_views.commentFunction,name='commentFunction'),
+    re_path(r'^like/(?P<image_id>\d+)', app_views.like, name='like'),
+    re_path(r'^allcomments/(?P<photo_id>\d+)$',app_views.allcomments,name='allcomments'),
+    re_path(r'^search/$',app_views.search,name='search'),
+    re_path(r'^feeds_profile/(?P<pk>\d+)$',app_views.users_profile,name='users_profile'),
+    re_path(r'^follow/(?P<user_id>\d+)$',app_views.follow,name='follow'),
+    re_path(r'^unfollow/(?P<user_id>\d+)$',app_views.unfollow,name='unfollow'),
+    re_path(r'^delete/(?P<photo_id>\d+)$',app_views.delete,name='delete'),
 ]
