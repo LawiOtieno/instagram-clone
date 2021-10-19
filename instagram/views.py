2e6eae89286044a008a6b1dd19193de26000ccac
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-19 07:20:02 +0300

    created views registration

--- instagram/views.py
+++ instagram/views.py
@@ -26,7 +26,7 @@ def index(request):
   return render (request,'index.html',{"photos":photos,"comment_form":comment_form,"post":post_form,"all_users":all_users})
 
 
-@login_required
+# @login_required
 def post(request):
   if request.method == 'POST':
     post_form = postPhotoForm(request.POST,request.FILES) 
@@ -41,4 +41,27 @@ def post(request):
   return render(request,'post.html',{"post_form":post_form})
 
 
-  
+def detail(request,photo_id):
+  current_user = request.user
+  try:
+    photo = get_object_or_404(Image, pk = photo_id)
+  except ObjectDoesNotExist:
+    raise Http404()
+  return render(request, 'photo_details.html', {'photo':photo,'current_user':current_user})
+
+
+def register(request):
+  if request.method == 'POST':
+    form = Registration(request.POST)
+    if form.is_valid():
+      form.save()
+      email = form.cleaned_data['email']
+      username = form.cleaned_data.get('username')
+
+      messages.success(request,f'Account for {username} created,you can now login')
+      return redirect('login')
+  else:
+    form = Registration()
+  return render(request,'registration/registration_form.html',{"form":form})
+
+
